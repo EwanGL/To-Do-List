899e9e701e26acbe571869786f5f975a60731f35
EwanGL <ewan.grignoux56@gmail.com>
2020-07-12 09:20:57 +0200

    Regroup tasks in differents folders

--- main.py
+++ main.py
@@ -4,10 +4,15 @@
 import sys
 import csv
 from tkinter import *
+from tkinter import ttk
 
 #Variables
 backgroung_color = '#2E2E2E'
+green = '#298A08'
+red ='#DF0101'
 list_of_tasks = []
+list_of_folders = ['None']
+list_of_folders_button = []
 
 #Functions
 def ReadCSVFile(File):
@@ -23,18 +28,22 @@ row_count = lambda : sum(1 for row in Tasks)
 
 def NewTaskDisplay():
     #This function create the display for save a new task
+    global list_of_folders
     def SaveTask():
         name = str(Name.get())
         description = str(Description.get())
+        folder = str(Folder.get())
         
-        new_line = [name, description]
+        new_line = [name, description, folder]
         
         with open ('Tasks.csv', 'a+', newline='', encoding='utf-8') as f:
             csv_writer = csv.writer(f)
             csv_writer.writerow(new_line)
+        
         Refresh()
         window.destroy()
 
+    
 
     window = Toplevel()
     window.title('Add a new task')
@@ -46,11 +55,19 @@ def NewTaskDisplay():
     description_task = StringVar()
     description_task.set('Description of the new task')
 
+    
+    Tasks = ReadCSVFile('Tasks.csv')
+    for i in range(len(Tasks)):
+        if Tasks[i]['Folder'] not in list_of_folders:
+            list_of_folders.append(Tasks[i]['Folder'])
+
     Name = Entry(window, textvariable = name_task, bg = backgroung_color, fg = 'white', width = 30, font = (20))
     Name.pack(padx=5, pady=5)
     Description = Entry(window, textvariable = description_task, bg = backgroung_color, fg = 'white', width = 30, font = (20))
     Description.pack(padx=5, pady=5)
-    Save = Button(window, text='Save Task', bg = backgroung_color, fg='white', font = (20), command = SaveTask)
+    Folder = ttk.Combobox(window, values=list_of_folders, width=30)
+    Folder.pack(padx=5, pady=5)
+    Save = Button(window, text='Save Task', bg = green, fg='white', font = (20), command = SaveTask)
     Save.pack(padx=5, pady=5)
 
 def ClickTask(i, liste):
@@ -58,9 +75,13 @@ def ClickTask(i, liste):
 
 def ShowTask(i, liste):
     #This function show the description of tasks ,in a new display, when we click on it.
+    global list_of_tasks
     def Done():
+        #This function delete the open task
+        list_of_folders.remove(liste[i]['Folder'])
+        
         with open(f'Tasks.csv', 'w', newline='', encoding = 'utf-8')as f:
-            titres = ['Name', 'Description']
+            titres = ['Name', 'Description', 'Folder']
             ecrivain = csv.DictWriter(f, fieldnames=titres)
             ecrivain.writeheader()
             for (index, compartiment) in enumerate(liste):
@@ -68,40 +89,74 @@ def ShowTask(i, liste):
                     continue
 
                 ecrivain.writerow(compartiment)
+        
+        
         Refresh()
         infos.destroy()
     infos = Toplevel()
     infos.title = liste[i]['Name']
-    infos.geometry('720x480+300+300')
+    infos.geometry('480x200+700+300')
     infos.configure(bg=backgroung_color)
-    description_label = Label(infos, text=liste[i]['Description'], fg='white', bg=backgroung_color, wraplength = 100)
+    description_label = Label(infos, text=liste[i]['Description'], fg='white', font=(20), bg=backgroung_color, wraplength = 300)
     description_label.pack(padx=5, pady=5)
-    done = Button(infos, text='Done', command=Done, fg='white', bg=backgroung_color)
+    done = Button(infos, text='Done', command=Done, fg='white', bg=red)
     done.pack(padx=5, pady=5, side = BOTTOM)
 
 def Refresh():
     #This function, refresh the frame with buttons(tasks) in it.
+    global list_of_folders, list_of_folders_button
     Tasks_refresh = ReadCSVFile('Tasks.csv')
-    for btn in list_of_tasks:
-        btn.pack_forget()
+    
     for i in range(len(Tasks_refresh)):
-        list_of_tasks.append(Button(frame2))
-        list_of_tasks[i].configure(text=Tasks_refresh[i]['Name'], command = ClickTask(i, Tasks_refresh), bg = backgroung_color, fg='white', font=(20))
-        list_of_tasks[i].pack(fill=X, padx=5, pady=5)
+        if Tasks_refresh[i]['Folder'] not in list_of_folders:
+            list_of_folders.append(Tasks_refresh[i]['Folder'])
+
+    for btn in list_of_tasks:
+        btn.forget()
+
+    def ClickFolder(folder):
+        return lambda: FrameTasks(folder)
+    
+    def FrameTasks(folder):
+        #This function upgrade the frame2 with tasks in folder that the user has selected.
+        j = -1
+        for i in range(len(Tasks_refresh)):
+            if Tasks_refresh[i]['Folder'] == folder:
+                list_of_tasks.append(Button(frame2))
+                j+=1
+                list_of_tasks[j].configure(text=Tasks_refresh[i]['Name'], command = ClickTask(i, Tasks_refresh), bg = backgroung_color, fg='white', font=(20))
+                list_of_tasks[j].pack(fill=X, padx=5, pady=5)
+    
+    for button in list_of_folders_button:
+        button.forget()
+
+    for i in range (len(list_of_folders)):
+        list_of_folders_button.append(Button(frame3))
+        if list_of_folders[i] == 'None':
+            list_of_folders_button[i].configure(text='None', command=ClickFolder('None'), bg=backgroung_color, fg='white', font=(20))
+        else:
+            list_of_folders_button[i].configure(text=Tasks_refresh[i]['Folder'], command=ClickFolder(Tasks_refresh[i]['Folder']), bg=backgroung_color, fg='white', font=(20))
+        list_of_folders_button[i].pack(fill=X, padx=5, pady=5)
 
 #Root, the principal display
 root = Tk()
 root.title('To Do List')
-root.geometry('1080x720+200+75')
+root.geometry('1280x750+300+75')
 root.configure(bg = backgroung_color)
 
 #Element(s) of root
 frame1 = Frame(root, bg = backgroung_color, height=30)
 frame1.pack(side=TOP, fill=X)
-frame2 = LabelFrame(root, text='My Tasks', fg = 'white',font=(20), bg = backgroung_color, width = 900, height= 690)
-frame2.pack(anchor=SE, padx=5, pady=5, expand=YES)
+
+frame2 = LabelFrame(root, text='My Tasks', fg = 'white',font=(20), bg = backgroung_color, width = 1100, height= 690)
+frame2.pack(side=RIGHT, padx=5, pady=5, expand=YES)
 frame2.pack_propagate(False)
-New = Button(frame1, text='New Task', bg = backgroung_color, fg='white', command = NewTaskDisplay, font = (20))
+
+frame3 = LabelFrame(root, text='My Folders', fg='white', font=(20), bg=backgroung_color, width=170, height=690)
+frame3.pack(side=LEFT, padx=5, pady=5)
+frame3.pack_propagate(False)
+
+New = Button(frame1, text='New Task', bg = green, fg='white', command = NewTaskDisplay, font = (20))
 New.pack(side=LEFT)
 
 Refresh()
